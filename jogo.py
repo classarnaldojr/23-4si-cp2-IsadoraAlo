1d54a3706170a4bdc9804b9f965d61372bde8f05
Isadora <rm84246@fiap.com.br>
2023-04-21 12:46:48 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,29 +1,41 @@
 import cv2
+import numpy as np
 
 # cor padrão para desenhos e textos.
 roxinho = (115, 50, 168)
 
-#converte a imagem para cinza.
-def imagem_video_gray(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    return gray
+#converte a imagem para RGB
+def imagem_video_rgb(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#converte a imagem para HSV
+def imagem_video_hsv(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+#acha o range de cores das mãos
+def achar_cores(hsv):
+    mask_hsv_rosa = cv2.inRange(hsv, np.array([0, 20, 0]), np.array([255, 255, 255]))
+    mask_hsv_amarelo = cv2.inRange(hsv, np.array([0, 30, 0]), np.array([255, 255, 255]))
+    return cv2.bitwise_or(mask_hsv_amarelo, mask_hsv_rosa)
+
+#define os contornos da imagem
+def acha_contorno(hsv):
+    contornos, _ = cv2.findContours(achar_cores(hsv), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return sorted(contornos, key=cv2.contourArea, reverse= True)
+
+#retorna o valor numérico da área do contorno
+def acha_area_contorno(contorno):
+    return cv2.contourArea(contorno)
+
+#desenha o contorno em torno da imagem
+def desenha_contorno(rgb, contornos):
+    contornos_img = rgb.copy()
+    return cv2.drawContours(contornos_img, [contornos[0], contornos[1]], -1, roxinho, 5)
 
 #identifica a imagem de movimento, e imprime o texto na tela
-def identifica_movimento(img, ref, texto):
-    #Identifica o caminho do movimento
-    movimentoPath = cv2.data.haarcascades + ref
-
-    #Classificador do movimento
-    movimentoCascade = cv2.CascadeClassifier(movimentoPath) 
-
-    #detecta o movimento na imagem
-    movimento = movimentoCascade.detectMultiScale(img, scaleFactor = 1.2, minNeighbors = 5)
-
-    # Desenha um retangulo nos movimentos detectados, e imprime o texto correspondente ao movimento da tela
-    for (x, y, w, h) in movimento:
-        cv2.rectangle(img, (x, y), (x+w, y+h), roxinho, 4)
-        cv2.putText(img, str(texto), (x, y+50), cv2.FONT_HERSHEY_SIMPLEX, 1, roxinho, 2, cv2.LINE_AA)
-    return movimento
+#def identifica_movimento(img, ref, minValue, maxValue, texto):
+#identifica o jogador ganhador
+#def identifica_vitoria:
 
 #Cria uma janela onde o vídeo irá rodar.
 cv2.namedWindow("checkpoint")
@@ -37,20 +49,30 @@ else:
     rval = False
 
 while rval:
-    #Pega o frame atual e o transforma em tom de cinza.
-    gray = imagem_video_gray(frame)
 
-    #Identifica o movimento pedra
-    identifica_movimento(gray, "pedra.png",'Pedra')
+    #converte a imagem para RGB
+    rgb = imagem_video_rgb(frame)
+    #converte a imagem para HSV
+    hsv = imagem_video_hsv(frame)
 
-    #Identifica o movimento papel
-    identifica_movimento(gray, "papel.png",'Papel')
+    #acha o contorno das mãos
+    contornos = acha_contorno(hsv)
+    #desenha o contorno das mãos
+    desenho_contornos = desenha_contorno(rgb, contornos)
+
+    #calcula o centro de massa do primeiro jogador
+    print(acha_area_contorno(contornos[0]))
+    print(acha_area_contorno(contornos[1]))
+    #calcula o centro de massa do segundo jogador
+    # #Identifica o movimento pedra
     
-    #Identifica o movimento tesoura
-    identifica_movimento(gray, "tesoura.png",'Tesoura')    
+    #Identifica o movimento papel
+    
+    # #Identifica o movimento tesoura
+    
 
     #Exibe a imagem scriptada.
-    cv2.imshow("checkpoint", frame)
+    cv2.imshow("checkpoint", desenho_contornos)
 
     #Leitura do vídeo, frame a frame.
     rval, frame = video.read()
