53376540ff15f54b444d011a1c8ba9f5741d8c8a
Isadora <rm84246@fiap.com.br>
2023-04-21 16:43:28 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -4,15 +4,15 @@ import numpy as np
 # cor padrão para desenhos e textos.
 roxinho = (115, 50, 168)
 
-#converte a imagem para RGB
+#Converte a imagem para RGB
 def imagem_video_rgb(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-#converte a imagem para HSV
+#Converte a imagem para HSV
 def imagem_video_hsv(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-#acha o range de cores das mãos
+#Acha o range de cores das mãos
 def achar_cores(hsv):
     mask_hsv_rosa = cv2.inRange(hsv, np.array([0, 20, 0]), np.array([255, 255, 255]))
     mask_hsv_amarelo = cv2.inRange(hsv, np.array([0, 30, 0]), np.array([255, 255, 255]))
@@ -27,13 +27,24 @@ def acha_contorno(hsv):
 def acha_area_contorno(contorno):
     return cv2.contourArea(contorno)
 
-#desenha o contorno em torno da imagem
+#Desenha o contorno em torno da imagem
 def desenha_contorno(rgb, contornos):
     contornos_img = rgb.copy()
     return cv2.drawContours(contornos_img, [contornos[0], contornos[1]], -1, roxinho, 5)
 
-#identifica a imagem de movimento, e imprime o texto na tela
-#def identifica_movimento(img, ref, minValue, maxValue, texto):
+#identifica o tipo movimento
+def identifica_movimento(area):
+    if(50000 < area < 52000):
+        return 'Pedra'
+    if(63000  < area < 64000):
+        return 'Papel'
+    if(48000 < area < 49500):
+        return 'Tesoura'
+
+#Imprime na tela um texto
+def exibe_texto(txt):
+    return cv2.putText(desenho_contornos, str(txt),(50,50), cv2.FONT_HERSHEY_SIMPLEX,1, roxinho,2,cv2.LINE_AA)
+
 #identifica o jogador ganhador
 #def identifica_vitoria:
 
@@ -50,29 +61,32 @@ else:
 
 while rval:
 
-    #converte a imagem para RGB
+    #Converte a imagem para RGB
     rgb = imagem_video_rgb(frame)
-    #converte a imagem para HSV
+    #Converte a imagem para HSV
     hsv = imagem_video_hsv(frame)
 
-    #acha o contorno das mãos
+    #Acha o contorno das mãos
     contornos = acha_contorno(hsv)
-    #desenha o contorno das mãos
+    #Desenha o contorno das mãos
     desenho_contornos = desenha_contorno(rgb, contornos)
 
-    #calcula o centro de massa do primeiro jogador
-    print(acha_area_contorno(contornos[0]))
-    print(acha_area_contorno(contornos[1]))
-    #calcula o centro de massa do segundo jogador
-    # #Identifica o movimento pedra
-    
-    #Identifica o movimento papel
-    
-    # #Identifica o movimento tesoura
-    
+    #Calcula a area de contorno do primeiro jogador
+    area_1 = acha_area_contorno(contornos[0])
+    #Calcula a area de contorno do segundo jogador
+    area_2 = acha_area_contorno(contornos[1])
+
+    print(identifica_movimento(area_1))
+    #Identifica o movimento do primeiro jogador
+    movimento_1 = identifica_movimento(area_1)
+    #Identifica o movimento do segundo jogador
+    movimento_2 = identifica_movimento(area_2)
+
+    #Imprime os movimentos realizado na tela
+    ala = exibe_texto(movimento_1+' x '+movimento_2)
 
     #Exibe a imagem scriptada.
-    cv2.imshow("checkpoint", desenho_contornos)
+    cv2.imshow("checkpoint", ala)
 
     #Leitura do vídeo, frame a frame.
     rval, frame = video.read()
